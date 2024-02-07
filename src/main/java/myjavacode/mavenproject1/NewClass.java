d121913353201b4833cbd02bf41fbfbc3ccf908a
dinakit <privet>
2024-02-07 03:34:01 +0300

    Initial commit

---
+++ src/main/java/myjavacode/mavenproject1/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package myjavacode.mavenproject1;
+
+/**
+ *
+ * @author Dina
+ */
+// Это пока что всё, чему я успела научиться за 2 дня. Изучение Java 
+// есть в программе курсов на GeekBrains, на котором я прохожу обучение
+// на тестировщика. Он начнётся 14 февраля)
